c7fe150f0031060d5df64845b9389a26d0d59393
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-12-12 12:01:09 +0200

    backend barebones for mysql

---
+++ api/db/index.js
@@ -0,0 +1,34 @@
+var mysql = require("mysql");
+require("dotenv");
+
+var db_config = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+};
+
+var con;
+
+const connect = () => {
+  con = mysql.createConnection(db_config);
+
+  con.connect((err) => {
+    if (err) {
+      console.log(`error when connecting to db: ${err}`);
+      setTimeout(connect, 2000);
+    }
+  });
+
+  con.on(`error`, (err) => {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      connect();
+    } else {
+      throw err;
+    }
+  });
+};
+
+connect();
+
+module.exports = con;
---
+++ api/routes/sport.js
@@ -0,0 +1,14 @@
+const { Router, Request, Response } = require("express");
+const route = Router();
+
+module.exports = (app, connection) => {
+  app.use("/sport", route);
+
+  route.get("/reservations", (req, res) => {
+    connection.query("SELECT * FROM rezervacija", (err, result) => {
+      if (err) throw err;
+      console.log("Result: " + result);
+      res.json(result);
+    });
+  });
+};
