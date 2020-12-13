b9b580d07e54614ba03c3fa9423f94adc6624c69
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-12-13 20:01:21 +0200

    mysql fix with createPool

--- api/db/index.js
+++ api/db/index.js
@@ -1,34 +1,14 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 require("dotenv");
 
 var db_config = {
+  connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB,
 };
-var con;
-const connect = () => {
-  var con = mysql.createConnection(db_config);
 
-  con.connect((err) => {
-    if (err) {
-      console.log(`error when connecting to db: ${err}`);
-      setTimeout(connect, 2000);
-    }
-  });
+var con = mysql.createPool(db_config);
 
-  con.on(error, (err) => {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.log(err);
-    } else {
-      throw err;
-    }
-  });
-};
-connect();
-
-setInterval(function () {
-  con.query("SELECT 1");
-}, 5000);
 module.exports = con;
